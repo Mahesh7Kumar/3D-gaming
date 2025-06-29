4ac7fdff8de31c17f4cb3ce27c1e0bdfb346ce76
Mahesh7Kumar <kumarmahesh47223@gmail.com>
2025-06-29 11:52:28 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Header from './components/header'
+import Hero from './components/Hero'
+import Characters from './components/Characters'
+import Arena from './components/Arena'
+import Footer from './components/Footer'
+
+const App = () => {
+  return (
+  <div>
+    <Header />
+    <Hero />
+    <Characters />
+    <Arena />
+    <Footer />
+  </div>
+  )
+}
+
+export default App
